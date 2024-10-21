bdd2eaaf12692e989da96c7498739db3f89ee90c
jocile <jocile@gmail.com>
2024-10-21 15:05:19 -0300

    Saque com condicional simples

---
+++ Conta-bancaria/saque.py
@@ -0,0 +1,13 @@
+'''
+Receba o valor do saque e compare com o saldo da conta bancária
+'''
+saldo_inicial = 1000
+saldo = saldo_inicial
+saque = float(input('Digite o valor do saque: '))
+if saque <= saldo_inicial:
+    print('Saque realizado com sucesso!')
+    saldo = saldo_inicial - saque
+else:
+    print('Saldo insuficiente!')
+
+print(f'Saldo atual: {saldo}')
