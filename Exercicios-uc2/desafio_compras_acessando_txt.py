17120e1e33a4d88d79d4b3d1b343cf9416ec9a38
jocile <jocile@gmail.com>
2024-11-04 15:10:50 -0300

    Desafio compras usando funções

--- Exercicios-uc2/desafio_compras_acessando_txt.py
+++ Exercicios-uc2/desafio_compras_acessando_txt.py
@@ -4,7 +4,7 @@ Esse desafio consiste em somar os itens de um lista de compras.
 Site do desafio: https://jocile.com/Programador/Desafios/Desafio-total-de-compras
 Criado a partir do desafio_compras em https://github.com/IllgnerPeixoto/Aula_py/
 
-- [ ] Organizar os códigos em funções
+- [X] Organizar os códigos em funções
 - [ ] Criar uma função para salvar a lista e o total em um arquivo txt.
 - [ ] Criar uma nova opção no menu para salvar a lista.
 
@@ -22,25 +22,43 @@ produtos = {'Arroz': 5.90,
             'Carne': 50.90,
             'Frango':20.00,}
 
-
-print('Produtos Disponíveis:')
-for produto, valor in produtos.items():
-    print(f"{produto}: R$ {valor}")
-
-# Carrinho
 carrinho = []
-total_compras = 0.0
 
-while True:
-    adicione = input("Digite o nome do produto, use 'x' para finalizar a compra: ")
-    if adicione == 'x': break
-    elif adicione in produtos:
+def mostrar_produtos():
+    print('Produtos Disponíveis:')
+    for produto, valor in produtos.items():
+        print(f"{produto}: R$ {valor}")
+
+def adicionar_produto(total_compras):
+    adicione = input("Digite o nome do produto: ")
+    if adicione in produtos:
         carrinho.append(adicione) #adiciona o produto ao carrinho
         total_compras += produtos[adicione] #soma o valor doso produtos adicionados
         print(f'{adicione} adicionado ao carrinho. Total até o momento: R$ {total_compras}')
     else:
         print("Produto não encontrado. Tente novamente.")
+    return total_compras
 
-#Exibindo o Total das Compras depois que apertar 'x'
-print("\nProdutos escolhidos:", ', '.join(carrinho))
-print(f"Total da compra: R$ {total_compras}")
+def mostrar_menu():
+    total_compras = 0.0
+    menu = """
+    Escolha uma opção dos produtos: 
+    m - (Mostrar); a - (Adicionar); c (Carrinho); f - (Finalizar Compra)
+    """
+    while True:
+        opcao = input(menu)
+        if opcao == 'm':
+            mostrar_produtos()
+        elif opcao == 'a':
+            total_compras = adicionar_produto(total_compras)
+        elif opcao == 'c':
+            print("Produtos no carrinho:")
+            for produto in carrinho:
+                print(produto)
+        elif opcao == 'f':
+            #Exibindo o Total das Compras depois que apertar 'x'
+            print("\nProdutos escolhidos:", ', '.join(carrinho))      
+            print(f"Total da compra: R$ {total_compras}")
+            break
+
+mostrar_menu()
